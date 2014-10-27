1c0b82130925ea2dc9a5d33084769788506e0372
Lucas Arnström <lucasarnstrom@gmail.com>
2014-10-27 16:48:10 +0100

    most of Room.java finished

--- src/generalobjects/Room.java
+++ src/generalobjects/Room.java
@@ -4,23 +4,69 @@ import java.util.HashSet;
 
 public class Room {
 
-    private final String NAME;
+    class Door {
+        private final Room    otherside;
+        private       boolean is_locked;
 
-    private HashSet<Room> rooms = null;
+        public Door(Room r, boolean lock_status) {
+            otherside = r;
+            is_locked = lock_status;
+        }
 
-    private boolean north_door_unlocked = false;
-    private boolean east_door_unlocked  = false;
-    private boolean south_door_unlocked = false;
-    private boolean west_door_unlocked  = false;
+        public boolean isLocked() {
+            return is_locked;
+        }
 
-    public Room(String name) {
-        this.NAME = name;
+        public void toggleLock() {
+            is_locked = !is_locked;
+        }
+
+        public Room getOtherSide() {
+            return otherside;
+        }
     }
 
-    
+    private final String NAME;
+    private final String DESCRIPTION;
 
-    public Room setDoorlockNorth() {
-        this.north_door_unlocked = !this.north_door_unlocked;
-        return this;
+    private HashSet<Door>   exits   = new HashSet<Door>();
+    private HashSet<Player> players = new HashSet<Player>();
+    private HashSet<NPC>    npcs    = new HashSet<NPC>();
+    private HashSet<Item>   items   = new HashSet<Item>();
+
+    public Room(String name, String description) {
+        this.NAME = name;
+        this.DESCRIPTION = description;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public void addExit(Room r, boolean locked) {
+        exits.add(new Door(r, locked));
+    }
+
+    public String[] getNameOfExits() {
+        String[] names = new String[exits.size()];
+
+        int i = 0;
+        for(Door d : exits) {
+            names[i++] = d.getOtherSide().getName();
+        }
+
+        return names;
+    }
+
+    public Player[] getPlayers() {
+        return players.toArray(new Player[players.size()]);
+    }
+
+    public NPC[] getNPCs() {
+        return npcs.toArray(new NPC[npcs.size()]);
+    }
+
+    public Item[] getItems() {
+        return items.toArray(new Item[items.size()]);
     }
 }
