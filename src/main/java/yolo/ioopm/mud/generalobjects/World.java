30dc83826157ebcd05168a2ba95d0390a3f58755
henrik.sommerland@gmail.com <henrik.sommerland@gmail.com>
2014-10-28 23:28:49 +0100

    "Started" working on the "main" class for the database

---
+++ src/main/java/yolo/ioopm/mud/generalobjects/World.java
@@ -0,0 +1,21 @@
+package yolo.ioopm.mud.generalobjects;
+
+import java.util.HashSet;
+
+/**
+ * 
+ * This is the "main" class of the database.
+ * 
+ * @author TheGrandmother
+ *
+ */
+public class World {
+	
+	
+	HashSet<Pc> player_set;
+	HashSet<Npc> npc_set;
+	HashSet<Room> room_set;
+	HashSet<Item> item_set;
+	
+	
+}
