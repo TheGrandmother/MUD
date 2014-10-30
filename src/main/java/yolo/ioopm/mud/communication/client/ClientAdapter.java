15b4c45858685cb646bb559216d7f62b52ef76ff
Lucas Arnström <lucasarnstrom@gmail.com>
2014-10-30 14:21:46 +0100

    dunno

--- src/main/java/yolo/ioopm/mud/communication/client/ClientAdapter.java
+++ src/main/java/yolo/ioopm/mud/communication/client/ClientAdapter.java
@@ -3,14 +3,6 @@ package yolo.ioopm.mud.communication.client;
 import yolo.ioopm.mud.communication.Adapter;
 import yolo.ioopm.mud.communication.Message;
 
-public class ClientAdapter implements Adapter {
-	@Override
-	public void sendMessage(Message message) throws CommunicationError {
-
-	}
-
-	@Override
-	public Message pollForMessage() throws CommunicationError {
-		return null;
-	}
+public class ClientAdapter extends Adapter {
+	
 }
--- src/main/java/yolo/ioopm/mud/communication/server/threads/ServerMessageListener.java
+++ src/main/java/yolo/ioopm/mud/communication/server/threads/ServerMessageListener.java
@@ -3,14 +3,14 @@ package yolo.ioopm.mud.communication.server.threads;
 import yolo.ioopm.mud.communication.Message;
 import yolo.ioopm.mud.communication.server.ClientConnection;
 
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ServerMessageListener extends Thread {
 
 	private final ConcurrentHashMap<String, ClientConnection> connections;
-	private final ConcurrentLinkedQueue<Message>      inbox;
+	private final ConcurrentLinkedQueue<Message>              inbox;
 
 	public ServerMessageListener(ConcurrentHashMap<String, ClientConnection> connections, ConcurrentLinkedQueue<Message> inbox) {
 		this.connections = connections;
@@ -19,6 +19,19 @@ public class ServerMessageListener extends Thread {
 
 	@Override
 	public void run() {
-		//TODO sleep the thread after every iteration over the box
+		while(true) {
+
+			for(Map.Entry<String, ClientConnection> entry : connections.entrySet()) {
+
+			}
+
+			try {
+				Thread.sleep(200);
+			}
+			catch(InterruptedException e) {
+				//TODO unhandled exception
+				e.printStackTrace();
+			}
+		}
 	}
 }
