41ae286eb31de35cae1afd651822c85c04117a38
TheGrandmother <henrik.sommerland@gmail.com>
2014-11-13 14:25:07 +0100

    Added serious error type

---
+++ src/main/java/yolo/ioopm/mud/communication/messages/server/SeriousErrorMessage.java
@@ -0,0 +1,13 @@
+package yolo.ioopm.mud.communication.messages.server;
+
+import yolo.ioopm.mud.communication.Message;
+import yolo.ioopm.mud.communication.MessageType;
+
+public class SeriousErrorMessage extends Message {
+
+	public SeriousErrorMessage(String receiver, String message) {
+		super(receiver, "server", MessageType.SERIOUS_ERROR, null, message);
+
+	}
+
+}
