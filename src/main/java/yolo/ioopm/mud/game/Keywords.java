cf248adeb5fa7760326bb940535344791a4d0837
Lucas Arnström <lucasarnstrom@gmail.com>
2014-11-19 12:55:38 +0100

    Made all variables public instead of package local

--- src/main/java/yolo/ioopm/mud/game/Keywords.java
+++ src/main/java/yolo/ioopm/mud/game/Keywords.java
@@ -2,20 +2,20 @@ package yolo.ioopm.mud.game;
 
 public abstract class Keywords {
 
-	static final String MAGIC_MOVE    = "move";
-	static final String ECHO          = "echo";
-	static final String SAY           = "say";
-	static final String SAY_REPLY     = "say";
-	static final String RETURN        = "return";
-	static final String WHISPER       = "whisper";
-	static final String WHISPER_REPLY = "whisper_return";
-	static final String LOOK          = "look";
-	static final String LOOK_REPLY    = "look_reply";
-	static final String MOVE          = "move";
-	static final String MOVE_REPLY    = "move_reply";
-	static final String TAKE          = "take";
-	static final String TAKE_REPLY    = "take_reply";
-	static final String DROP          = "drop";
-	static final String DROP_REPLY    = "drop_reply";
+	public static final String MAGIC_MOVE    = "move";
+	public static final String ECHO          = "echo";
+	public static final String SAY           = "say";
+	public static final String SAY_REPLY     = "say";
+	public static final String RETURN        = "return";
+	public static final String WHISPER       = "whisper";
+	public static final String WHISPER_REPLY = "whisper_return";
+	public static final String LOOK          = "look";
+	public static final String LOOK_REPLY    = "look_reply";
+	public static final String MOVE          = "move";
+	public static final String MOVE_REPLY    = "move_reply";
+	public static final String TAKE          = "take";
+	public static final String TAKE_REPLY    = "take_reply";
+	public static final String DROP          = "drop";
+	public static final String DROP_REPLY    = "drop_reply";
 
 }
