cf51b30c8d234fbdfeca676c81605b6e4e837a5f
henrik.sommerland@gmail.com <henrik.sommerland@gmail.com>
2014-11-22 19:45:02 +0100

    Added NotificationMessage class to handle notifications i.e broadcasted messages

---
+++ src/main/java/yolo/ioopm/mud/communication/messages/server/NotifactionMesssage.java
@@ -0,0 +1,19 @@
+package yolo.ioopm.mud.communication.messages.server;
+
+import yolo.ioopm.mud.communication.Message;
+import yolo.ioopm.mud.communication.MessageType;
+/**
+ * 
+ * Messages for notifications i.e broadcasts.
+ * 
+ * @author TheGrandmother
+ *
+ */
+public class NotifactionMesssage extends Message {
+
+	public NotifactionMesssage(String receiver, String message) {
+		super(receiver, "server", MessageType.NOTIFICATION, null, new String[]{message});
+		// TODO Auto-generated constructor stub
+	}
+
+}
