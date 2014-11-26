66751d1612a13a31618e8860f50856ebd7be827f
Lucas Arnström <lucasarnstrom@gmail.com>
2014-11-26 14:35:44 +0100

    Server now has its own formatter

---
+++ src/main/java/yolo/ioopm/mud/logger/ServerConsoleFormatter.java
@@ -0,0 +1,31 @@
+package yolo.ioopm.mud.logger;
+
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class ServerConsoleFormatter extends Formatter {
+
+	public String format(LogRecord record) {
+		StringBuilder sb = new StringBuilder();
+
+		// Date
+		Date d = new Date(record.getMillis());
+		sb.append('[');
+		sb.append(d.getHours()).append(':');
+		sb.append(d.getMinutes()).append(':');
+		sb.append(d.getSeconds());
+		sb.append(']');
+
+		// Level
+		sb.append('[').append(record.getLevel().getName()).append(']');
+
+		// Message
+		sb.append(' ').append(record.getMessage());
+
+		// New line
+		sb.append('\n');
+
+		return sb.toString();
+	}
+}
