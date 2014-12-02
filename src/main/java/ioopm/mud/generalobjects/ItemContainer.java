19fc98c1f700fc138b4f27322792c14630e56680
henrik.sommerland@gmail.com <henrik.sommerland@gmail.com>
2014-12-02 19:27:48 +0100

    Added comments

--- src/main/java/ioopm/mud/generalobjects/ItemContainer.java
+++ src/main/java/ioopm/mud/generalobjects/ItemContainer.java
@@ -1,35 +1,67 @@
 package ioopm.mud.generalobjects;
 
-
+/**
+ * This class is a simple clas used to indicate the precens of items in a room or an inventory
+ * 
+ * @author TheGrandmother
+ *
+ */
 public class ItemContainer{
+	/**
+	 * The item in the container
+	 */
 	Item item;
+	/**
+	 * How many of the item are in the container. May never be less than 1.
+	 */
 	int amount;
 	
+	/**
+	 * Creates a new item container with amount 1.
+	 * @param item The item contained in the item container
+	 */
 	public ItemContainer(Item item) {
 		this.item = item;
 		amount = 1;
 	}
 	
+	/**
+	 * Returns the item in the container.
+	 * @return the item in the container
+	 */
 	public Item getItem(){
 		return item;
 	}
 	
+	/**
+	 * Returns the name of the item in the container
+	 * @return The name of the item in the container
+	 */
 	public String getName(){
 		return item.getName();
 	}
 	
-	Item.Type getType(){
-		return item.getType();
-	}
-	
+	/**
+	 * Returns how many of the item are in the container
+	 * @return The amount of the item in the container
+	 */
 	public  int getAmount(){
 		return amount; 
 	}
 	
+	/**
+	 * Sets the amount of the item in the container
+	 * @param amount How many of the item are in the container
+	 */
 	public void setAmount(int amount){
 		this.amount = amount;
 	}
 	
+	/**
+	 * Adds items to the container
+	 * 
+	 * @param add How many items to add to the container. Must be larger than 0
+	 */
 	public void addAmount(int add){
 		if(add > 0){this.amount += add;}
 	}
