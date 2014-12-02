716aef5f72d5c3eb6fa5927481e3adf10ae279c6
henrik.sommerland@gmail.com <henrik.sommerland@gmail.com>
2014-12-02 16:20:52 +0100

    Fixed bug in take: The amount of an item in the room decreased even though the inventory was full

--- src/main/java/ioopm/mud/game/ItemInteraction.java
+++ src/main/java/ioopm/mud/game/ItemInteraction.java
@@ -49,17 +49,19 @@ public abstract class ItemInteraction {
 			return;
 		}
 		
-		if(!current_room.removeItem(item)){
-			adapter.sendMessage(new ErrorMessage(actor.getName(),"There is no " + item_name + " here."));
-			return;
-		}
-		
 		try {
 			actor.getInventory().addItem(item);
 		} catch (InventoryOverflow e) {
 			adapter.sendMessage(new ErrorMessage(actor.getName(), "Your inventory is full!"));
 			return;
 		}
+		
+		if(!current_room.removeItem(item)){
+			adapter.sendMessage(new ErrorMessage(actor.getName(),"There is no " + item_name + " here."));
+			return;
+		}
+		
+
 		
 		adapter.sendMessage(new ReplyMessage(actor.getName(), Keywords.TAKE_REPLY, "You picked up a/an " + item_name +"."));
 		GameEngine.broadcastToRoom(adapter, current_room, actor.getName() +" picked up " + item_name+".", actor.getName());
