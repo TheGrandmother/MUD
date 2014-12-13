7ea82a18709039cc5967b74ece0e6ce912ec1e1e
Lucas Arnström <lucasarnstrom@gmail.com>
2014-12-13 21:19:43 +0100

    Added some error checks to Main

--- src/main/java/ioopm/mud/Main.java
+++ src/main/java/ioopm/mud/Main.java
@@ -17,7 +17,54 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		// -- Logger set up --
+		// Set up the logger.
+		setUpLogger();
+
+		logger.fine("Initiating program!");
+
+		if(args.length != 1) {
+			System.out.println("Incorrect usage! Please provide if the game should run as client or server!");
+			System.out.println("Example usage: java -jar MUD.jar client");
+			return;
+		}
+
+		switch(args[0].toLowerCase()) {
+
+			case "client":
+				logger.fine("Initiating client...");
+
+				try {
+					setRootFormatter(new ClientConsoleFormatter(), Level.INFO);
+					new Client();
+				}
+				catch(Exception e) {
+					logger.log(Level.SEVERE, e.getMessage(), e);
+				}
+				break;
+
+			case "server":
+				logger.fine("Initiating server...");
+
+				try {
+					setRootFormatter(new ServerConsoleFormatter(), Level.ALL);
+					new Server();
+				}
+				catch(Exception e) {
+					logger.log(Level.SEVERE, e.getMessage(), e);
+				}
+				break;
+
+			default:
+				System.out.println("Incorrect choice! Please choose either client or server!");
+				return;
+		}
+	}
+
+	/**
+	 * Sets up the logger.
+	 * Creates log file and sets the general log level.
+	 */
+	private static void setUpLogger() {
 		Logger root_logger = Logger.getLogger("");
 		root_logger.setLevel(Level.ALL);
 
@@ -35,47 +82,16 @@ public class Main {
 		log_file.setLevel(Level.ALL);
 
 		root_logger.addHandler(log_file);
-		// -- Logger set up finished --
-
-
-		logger.fine("Initiating program!");
-
-		switch(args[0].toLowerCase()) {
-
-			case "client":
-				logger.fine("Initiating client...");
-
-				try {
-					setConsoleFormatter(root_logger, new ClientConsoleFormatter(), Level.INFO);
-					new Client();
-				}
-				catch(Exception e) {
-					logger.log(Level.SEVERE, e.getMessage(), e);
-				}
-				break;
-
-			case "server":
-				logger.fine("Initiating server...");
-
-				try {
-					setConsoleFormatter(root_logger, new ServerConsoleFormatter(), Level.ALL);
-					new Server();
-				}
-				catch(Exception e) {
-					logger.log(Level.SEVERE, e.getMessage(), e);
-				}
-				break;
-		}
 	}
 
 	/**
 	 * Sets the formatter for the ConsoleHandlers of the defined logger.
-	 * @param logger - Logger to get the ConsoleHandlers from.
 	 * @param formatter - The formatter to use on the ConsoleHandlers.
 	 * @param level - The level to log.
 	 */
-	private static void setConsoleFormatter(Logger logger, Formatter formatter, Level level) {
-		Handler[] handlers = logger.getHandlers();
+	private static void setRootFormatter(Formatter formatter, Level level) {
+		Logger root = Logger.getLogger("");
+		Handler[] handlers = root.getHandlers();
 		for(Handler h : handlers) {
 			if(h instanceof ConsoleHandler) {
 				h.setFormatter(formatter);
