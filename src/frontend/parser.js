ec4e289ad05b09881d9d4226e3e62415a1ac699b
thegrandmother <henrik.sommerland@gmail.com>
2015-12-12 18:12:47 +0100

    Added all the actions :D

--- src/frontend/parser.js
+++ src/frontend/parser.js
@@ -29,6 +29,59 @@ function parseInput(inp){
 				console.log("Trying to wisper: \""+ msg + "\" to: " + recipient );
 				return makeRequest("whisper",[recipient,msg],user);
 
+			case "take":
+				arg = inp.split(" ").slice(1).join(" ");
+				console.log("trying to take: " + arg);
+				return makeRequest("take",[arg],user);
+				break;
+
+			case "drop":
+				arg = inp.split(" ").slice(1).join(" ");
+				console.log("trying to drop: " + arg);
+				return makeRequest("drop",[arg],user);
+				break;
+			
+			case "attack":
+				arg = inp.split(" ").slice(1).join(" ");
+				console.log("trying to attack: " + arg);
+				return makeRequest("attack",[arg],user);
+				break;
+			
+			case "equip":
+				arg = inp.split(" ").slice(1).join(" ");
+				console.log("trying to equip: " + arg);
+				return makeRequest("equip",[arg],user);
+				break;
+
+			case "examine":
+				arg = inp.split(" ").slice(1).join(" ");
+				console.log("trying to examine: " + arg);
+				return makeRequest("examine",[arg],user);
+				break;
+			
+			case "cs":
+				return makeRequest("cs",[],user);
+				
+				break;
+
+			
+			case "inventory":
+				return makeRequest("inventory",[],user);
+				
+				break;
+			
+			case "unequip":
+				return makeRequest("unequip",[],user);
+				
+				break;
+
+			case "quit":
+			case "logout":
+				appendToOut("You have left the game :/");
+				return logoutMessage();
+
+				break;
+			
 			default:
 				appendToOut("Pointless input");
 				return null;
