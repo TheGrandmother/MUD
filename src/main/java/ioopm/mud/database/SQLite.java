01f906e97c88031902b144bdad2071e959423326
Lucas Arnström <lucasarnstrom@gmail.com>
2015-12-14 14:18:59 +0100

    Almost done with table designs

--- src/main/java/ioopm/mud/database/SQLite.java
+++ src/main/java/ioopm/mud/database/SQLite.java
@@ -7,6 +7,43 @@ import java.sql.SQLException;
 
 public class SQLite {
 
+	// TABLES
+	final static String TABLE_PLAYER = "CREATE TABLE IF NOT EXISTS player ("+
+			"id INTEGER PRIMARY KEY, " +
+			"username TEXT NOT NULL, " +
+			"password_hash TEXT NOT NULL, " +
+			"hash_salt TEXT NOT NULL" +
+		")";
+
+	final static String TABLE_INVENTORY = "CREATE TABLE IF NOT EXISTS inventory (" +
+			"player_id INTEGER, " +
+			"volume INTEGER NOT NULL, " +
+			"max_volume INTEGER NOT NULL, " +
+			"FOREIGN KEY(player_id) REFERENCES player(id)" +
+		")";
+
+
+	final static String TABLE_ITEM = "CREATE TABLE IF NOT EXISTS item (" +
+			"item_id INTEGER PRIMARY KEY, " +
+			"level INTEGER NOT NULL, " +
+			"description TEXT NOT NULL, " +
+			"size INTEGER NOT NULL, " +
+			"dropable BOOLEAN NOT NULL" +
+		")";
+
+	final static String TABLE_CHARACTERSHEET = "CREATE TABLE IF NOT EXISTS character_sheet (" +
+			"player_id INTEGER, " +
+		")";
+
+	// M-N Relations
+	final static String INVENTORY_ITEM_RELATION = "CREATE TABLE IF NOT EXISTS inventory_item_relation (" +
+			"inv_id INTEGER, " +
+			"item_id INTEGER, " +
+			"amount INTEGER, " +
+			"FOREIGN KEY(inv_id) REFERENCES inventory(player_id), " +
+			"FOREIGN KEY(item_id) REFERENCES item(item_id)" +
+		")";
+
 	private final Connection database_connection;
 
 	public SQLite(File database_file) throws ClassNotFoundException, SQLException {
@@ -23,5 +60,6 @@ public class SQLite {
 	 * Does not overwrite already defined tables/relations.
 	 */
 	public void setupDatabase() {
+
 	}
 }
