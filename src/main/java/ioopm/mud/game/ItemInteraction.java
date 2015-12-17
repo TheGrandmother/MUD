TheGrandmother/MUD

commit 8b6185ec83c524db12ef529bcad1133feeaaf2fe

Moved CS and inventory to separate files

src/main/java/ioopm/mud/generalobjects/Character.java
@@ -2,8 +2,6 @@ package ioopm.mud.generalobjects;
 
 import ioopm.mud.generalobjects.items.Weapon;
 
-import java.util.HashSet;
-
 /**
  * This class specifies a Character. Will be extended by other classes for PC's and NPC's.
  * <p>
@@ -114,325 +112,6 @@ public abstract class Character extends Entity {
 	 */
 	public CharacterSheet getCs() {
 		return cs;
-	}
-
-
-	/**
-	 * The CharacterSheet contains information about the characters current status.
-	 * <p>
-	 * <b>NOTE:</b> hp denotes university credits which is so dumb its insane. This is due to the original specification being in Swedish. One day.... it shall be refactored.
-	 *
-	 * @author TheGrandmother
-	 */
-	public class CharacterSheet {
-		private final int BASE_HP = 30;
-		private final double HP_FACTOR = 1.5;
-		private final double HEALTH_FACTOR = 1.3;
-		private int hp;
-		private int health;
-		private int max_health;
-		private int level;
-
-		/**
-		 * Creates a new default character sheet with<p>
-		 * Level = 1<p>
-		 * Health = 50<p>
-		 * Max_health = 50<p>
-		 * Hp = 30<p>
-		 */
-		public CharacterSheet() {
-			this.hp = BASE_HP;
-			this.level = 1;
-			this.health = 50;
-			this.max_health = 50;
-			this.hp = 30;
-
-		}
-
-		/**
-		 * @return The hp (university credits) of the character
-		 */
-		public int getHp() {
-			return hp;
-		}
-
-		/**
-		 * sets the hp (university credits)
-		 *
-		 * @param hp
-		 */
-		public void setHp(int hp) {
-			this.hp = hp;
-		}
-
-		/**
-		 * Computes how many university credits left to the next level.<p>
-		 * <b>NOTE:</b> This needs to correspond with how {@link CharacterSheet#levelUp()} works.
-		 *
-		 * @return how many hp left until the new level is reached.
-		 */
-		public int hpToNextLevel() {
-			return (int) (BASE_HP * (HP_FACTOR * level)) - hp;
-		}
-
-		/**
-		 * Checks if the player can be leveled up.<p>
-		 * The mechanics of leveling will be described in the specs/Mechanics.txt file.<p>
-		 * If the level-up is successful the level will be incremented and a new max health will be set
-		 * and full health will be restored.
-		 *
-		 * @return true if the player leveled up. False otherwise.
-		 */
-		//TODO This seriously needs to be reworked!
-		public boolean levelUp() {
-			if(hp >= BASE_HP * (HP_FACTOR * level)) {
-				level++;
-				max_health = (int) (max_health * (HEALTH_FACTOR));
-				health = max_health;
-				return true;
-			}
-			return false;
-		}
-
-		/**
-		 * Adds or subtracts hp. Can't set hp to less than 0
-		 *
-		 * @param hp
-		 */
-		public void addHp(int hp) {
-			if(this.hp + hp < 0) {
-				this.hp = 0;
-			} else {
-				this.hp += hp;
-			}
-		}
-
-		/**
-		 * @return The characters health
-		 */
-		public int getHealth() {
-			return health;
-		}
-
-		/**
-		 * Tries to set the Characters health to the number given as argument.
-		 * If the argument given is greater than the max health of the character
-		 * the character will get its maximum health.
-		 *
-		 * @param health
-		 */
-		public void setHealth(int health) {
-			if(health + this.health > this.max_health) {
-				this.health = this.max_health;
-			} else {
-				this.health = health;
-			}
-		}
-
-		/**
-		 * Adds the amount of health. Which can also be negative.
-		 * <p>
-		 * If the new health is greater than the maximum health of the
-		 * character the characters health will be its maximum health.
-		 * If it where to be less than zero the characters health will be set to zero.
-		 *
-		 * @param amount
-		 */
-		public void addHealth(int amount) {
-			if(this.health + amount > this.max_health) {
-				this.health = this.max_health;
-			} else if(this.health + amount < 0) {
-				this.health = 0;
-			} else {
-				this.health += amount;
-			}
-		}
-
-		/**
-		 * Sets the maximum health
-		 *
-		 * @param max_health
-		 */
-		public void setMax_health(int max_health) {
-			this.max_health = max_health;
-		}
-
-		/**
-		 * gets the maximum health.
-		 *
-		 * @return
-		 */
-		public int getMaxHealth() {
-			return max_health;
-		}
-
-		/**
-		 * @return The current level of the character
-		 */
-		public int getLevel() {
-			return level;
-		}
-
-		/**
-		 * Sets the level of the character
-		 *
-		 * @param level
-		 */
-		public void setLevel(int level) {
-			this.level = level;
-		}
-
-
-	}
-
-	/**
-	 * The inventory contains and manages all of the items possessed by the Character.
-	 *
-	 * @author TheGrandmother
-	 */
-	public class Inventory {
-
-		private HashSet<ItemContainer> items;
-		private int volume;
-		private int max_volume = 10;
-
-		/**
-		 * Creates a new empty inventory with max_volume set to 10.
-		 */
-		public Inventory() {
-			volume = 0;
-			items = new HashSet<ItemContainer>();
-		}
-
-		/**
-		 * returns the set of ItemContainers
-		 *
-		 * @return The set of item containers.
-		 */
-		public HashSet<ItemContainer> getitems() {
-			return this.items;
-		}
-
-		/**
-		 * Tries to retrieve an item from the items set.
-		 * NOTE: this does not remove the item!
-		 *
-		 * @param name the name of the item
-		 * @return Returns null if no silly item was found
-		 */
-		public Item findItem(String name) {
-			for(ItemContainer i : items) {
-				if(i.getName().equals(name)) {
-					return i.getItem();
-				}
-			}
-			return null;
-
-		}
-
-		/**
-		 * Tries to remove the item. Either removes the {@link ItemContainer} completely or just decreases the amount.
-		 *
-		 * @param name The name of the item.
-		 * @return false if item does not exist
-		 */
-		public boolean removeItem(String name) {
-
-			for(ItemContainer i : items) {
-				if(i.getName().equals(name)) {
-					if(i.getAmount() == 1) {
-						volume -= i.getItem().getSize();
-						items.remove(i);
-						return true;
-					} else {
-						volume -= i.getItem().getSize();
-						i.setAmount(i.getAmount() - 1);
-						return true;
-					}
-				}
-			}
-
-			return false;
-		}
-
-
-		/**
-		 * Ads an item to the inventory. Creates a new {@link ItemContainer} or just dereases the amount.
-		 *
-		 * @param item item to be added
-		 * @throws InventoryOverflow gets thrown if item does not fit.
-		 */
-		public void addItem(Item item) throws InventoryOverflow {
-			for(ItemContainer i : items) {
-				if(item.getName().equals(i.getName())) {
-					if(item.getSize() + volume > max_volume) {
-						throw new InventoryOverflow();
-					} else {
-						volume += item.getSize();
-						i.setAmount(i.getAmount() + 1);
-						return;
-					}
-				}
-			}
-			if(item.getSize() + volume > max_volume) {
-				throw new InventoryOverflow();
-			} else {
-				volume += item.getSize();
-				items.add(new ItemContainer(item));
-				return;
-			}
-
-		}
-
-		/**
-		 * @return the maximum volume of the inventory
-		 */
-		public int getMax_volume() {
-			return max_volume;
-		}
-
-		/**
-		 * increases the capacity of the inventory
-		 *
-		 * @param max_volume new max volume
-		 */
-		public void setMax_volume(int max_volume) {
-			this.max_volume = max_volume;
-		}
-
-		/**
-		 * How much space is occupied
-		 *
-		 * @return the current volume of the inventory
-		 */
-		public int getVolume() {
-			return volume;
-		}
-
-		/**
-		 * sets the current volume
-		 *
-		 * @param volume
-		 */
-		public void setVolume(int volume) {
-			this.volume = volume;
-		}
-
-
-		@SuppressWarnings("serial")
-		/**
-		 *
-		 * This exception is to be thrown when an item does not fit into the inventory.
-		 *
-		 * @author TheGrandmother
-		 */
-		public class InventoryOverflow extends Exception {
-			public InventoryOverflow() {
-				super();
-			}
-		}
-
-
 	}
 
 

src/main/java/ioopm/mud/generalobjects/CharacterSheet.java
@@ -1,0 +1,169 @@
+package ioopm.mud.generalobjects;
+
+/**
+ * The CharacterSheet contains information about the characters current status.
+ * <p>
+ * <b>NOTE:</b> hp denotes university credits which is so dumb its insane. This is due to the original specification being in Swedish. One day.... it shall be refactored.
+ *
+ * @author TheGrandmother
+ */
+public class CharacterSheet {
+	private final int BASE_HP = 30;
+	private final double HP_FACTOR = 1.5;
+	private final double HEALTH_FACTOR = 1.3;
+	private int hp;
+	private int health;
+	private int max_health;
+	private int level;
+
+	/**
+	 * Creates a new default character sheet with<p>
+	 * Level = 1<p>
+	 * Health = 50<p>
+	 * Max_health = 50<p>
+	 * Hp = 30<p>
+	 */
+	public CharacterSheet() {
+		this.hp = BASE_HP;
+		this.level = 1;
+		this.health = 50;
+		this.max_health = 50;
+		this.hp = 30;
+
+	}
+
+	/**
+	 * @return The hp (university credits) of the character
+	 */
+	public int getHp() {
+		return hp;
+	}
+
+	/**
+	 * sets the hp (university credits)
+	 *
+	 * @param hp
+	 */
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	/**
+	 * Computes how many university credits left to the next level.<p>
+	 * <b>NOTE:</b> This needs to correspond with how {@link CharacterSheet#levelUp()} works.
+	 *
+	 * @return how many hp left until the new level is reached.
+	 */
+	public int hpToNextLevel() {
+		return (int) (BASE_HP * (HP_FACTOR * level)) - hp;
+	}
+
+	/**
+	 * Checks if the player can be leveled up.<p>
+	 * The mechanics of leveling will be described in the specs/Mechanics.txt file.<p>
+	 * If the level-up is successful the level will be incremented and a new max health will be set
+	 * and full health will be restored.
+	 *
+	 * @return true if the player leveled up. False otherwise.
+	 */
+	//TODO This seriously needs to be reworked!
+	public boolean levelUp() {
+		if(hp >= BASE_HP * (HP_FACTOR * level)) {
+			level++;
+			max_health = (int) (max_health * (HEALTH_FACTOR));
+			health = max_health;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Adds or subtracts hp. Can't set hp to less than 0
+	 *
+	 * @param hp
+	 */
+	public void addHp(int hp) {
+		if(this.hp + hp < 0) {
+			this.hp = 0;
+		} else {
+			this.hp += hp;
+		}
+	}
+
+	/**
+	 * @return The characters health
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	/**
+	 * Tries to set the Characters health to the number given as argument.
+	 * If the argument given is greater than the max health of the character
+	 * the character will get its maximum health.
+	 *
+	 * @param health
+	 */
+	public void setHealth(int health) {
+		if(health + this.health > this.max_health) {
+			this.health = this.max_health;
+		} else {
+			this.health = health;
+		}
+	}
+
+	/**
+	 * Adds the amount of health. Which can also be negative.
+	 * <p>
+	 * If the new health is greater than the maximum health of the
+	 * character the characters health will be its maximum health.
+	 * If it where to be less than zero the characters health will be set to zero.
+	 *
+	 * @param amount
+	 */
+	public void addHealth(int amount) {
+		if(this.health + amount > this.max_health) {
+			this.health = this.max_health;
+		} else if(this.health + amount < 0) {
+			this.health = 0;
+		} else {
+			this.health += amount;
+		}
+	}
+
+	/**
+	 * Sets the maximum health
+	 *
+	 * @param max_health
+	 */
+	public void setMax_health(int max_health) {
+		this.max_health = max_health;
+	}
+
+	/**
+	 * gets the maximum health.
+	 *
+	 * @return
+	 */
+	public int getMaxHealth() {
+		return max_health;
+	}
+
+	/**
+	 * @return The current level of the character
+	 */
+	public int getLevel() {
+		return level;
+	}
+
+	/**
+	 * Sets the level of the character
+	 *
+	 * @param level
+	 */
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+}

src/main/java/ioopm/mud/generalobjects/Inventory.java
@@ -1,0 +1,153 @@
+package ioopm.mud.generalobjects;
+
+import java.util.HashSet;
+
+/**
+ * The inventory contains and manages all of the items possessed by the Character.
+ *
+ * @author TheGrandmother
+ */
+public class Inventory {
+
+	private HashSet<ItemContainer> items;
+	private int                    volume;
+	private int max_volume = 10;
+
+	/**
+	 * Creates a new empty inventory with max_volume set to 10.
+	 */
+	public Inventory() {
+		volume = 0;
+		items = new HashSet<ItemContainer>();
+	}
+
+	/**
+	 * returns the set of ItemContainers
+	 *
+	 * @return The set of item containers.
+	 */
+	public HashSet<ItemContainer> getitems() {
+		return this.items;
+	}
+
+	/**
+	 * Tries to retrieve an item from the items set.
+	 * NOTE: this does not remove the item!
+	 *
+	 * @param name the name of the item
+	 * @return Returns null if no silly item was found
+	 */
+	public Item findItem(String name) {
+		for(ItemContainer i : items) {
+			if(i.getName().equals(name)) {
+				return i.getItem();
+			}
+		}
+		return null;
+
+	}
+
+	/**
+	 * Tries to remove the item. Either removes the {@link ItemContainer} completely or just decreases the amount.
+	 *
+	 * @param name The name of the item.
+	 * @return false if item does not exist
+	 */
+	public boolean removeItem(String name) {
+
+		for(ItemContainer i : items) {
+			if(i.getName().equals(name)) {
+				if(i.getAmount() == 1) {
+					volume -= i.getItem().getSize();
+					items.remove(i);
+					return true;
+				} else {
+					volume -= i.getItem().getSize();
+					i.setAmount(i.getAmount() - 1);
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * Ads an item to the inventory. Creates a new {@link ItemContainer} or just dereases the amount.
+	 *
+	 * @param item item to be added
+	 * @throws InventoryOverflow gets thrown if item does not fit.
+	 */
+	public void addItem(Item item) throws InventoryOverflow {
+		for(ItemContainer i : items) {
+			if(item.getName().equals(i.getName())) {
+				if(item.getSize() + volume > max_volume) {
+					throw new InventoryOverflow();
+				} else {
+					volume += item.getSize();
+					i.setAmount(i.getAmount() + 1);
+					return;
+				}
+			}
+		}
+		if(item.getSize() + volume > max_volume) {
+			throw new InventoryOverflow();
+		} else {
+			volume += item.getSize();
+			items.add(new ItemContainer(item));
+			return;
+		}
+
+	}
+
+	/**
+	 * @return the maximum volume of the inventory
+	 */
+	public int getMax_volume() {
+		return max_volume;
+	}
+
+	/**
+	 * increases the capacity of the inventory
+	 *
+	 * @param max_volume new max volume
+	 */
+	public void setMax_volume(int max_volume) {
+		this.max_volume = max_volume;
+	}
+
+	/**
+	 * How much space is occupied
+	 *
+	 * @return the current volume of the inventory
+	 */
+	public int getVolume() {
+		return volume;
+	}
+
+	/**
+	 * sets the current volume
+	 *
+	 * @param volume
+	 */
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+
+	@SuppressWarnings("serial")
+	/**
+	 *
+	 * This exception is to be thrown when an item does not fit into the inventory.
+	 *
+	 * @author TheGrandmother
+	 */
+	public class InventoryOverflow extends Exception {
+		public InventoryOverflow() {
+			super();
+		}
+	}
+
+
+}

commit 9ca0787484c0c439399330e3ab93ac5dc98ea7a7

Fixed build errors caused by 8b6185ec83c524db12ef529bcad1133feeaaf2fe

src/main/java/ioopm/mud/game/ItemInteraction.java
@@ -4,8 +4,6 @@ import ioopm.mud.communication.Adapter;
 import ioopm.mud.communication.messages.server.ErrorMessage;
 import ioopm.mud.communication.messages.server.ReplyMessage;
 import ioopm.mud.exceptions.EntityNotPresent;
-import ioopm.mud.generalobjects.Character.Inventory;
-import ioopm.mud.generalobjects.Character.Inventory.InventoryOverflow;
 import ioopm.mud.generalobjects.*;
 import ioopm.mud.generalobjects.items.Weapon;
 
@@ -51,7 +49,7 @@ public abstract class ItemInteraction {
 		if(current_room.hasItem(item_name)) {
 			try {
 				actor.getInventory().addItem(item);
-			} catch(InventoryOverflow e) {
+			} catch(Inventory.InventoryOverflow e) {
 				adapter.sendMessage(new ErrorMessage(actor.getName(), "Your inventory is full!"));
 				return;
 			}
@@ -177,7 +175,7 @@ public abstract class ItemInteraction {
 				actor.setWeapon(null);
 				adapter.sendMessage(new ReplyMessage(actor.getName(), Keywords.UNEQUIP_REPLY, "You have unequipped your weapon."));
 				return;
-			} catch(InventoryOverflow e) {
+			} catch(Inventory.InventoryOverflow e) {
 				adapter.sendMessage(new ErrorMessage(actor.getName(), "You don't have enough space in your inventory. " + w.getName() + " takes up " + w.getSize() +
 					" units of space but you only have " + (actor.getInventory().getMax_volume() - actor.getInventory().getVolume()) + " free."));
 				return;

src/main/java/ioopm/mud/game/Movement.java
@@ -5,11 +5,7 @@ import ioopm.mud.communication.messages.server.ErrorMessage;
 import ioopm.mud.communication.messages.server.ReplyMessage;
 import ioopm.mud.communication.messages.server.SeriousErrorMessage;
 import ioopm.mud.exceptions.EntityNotPresent;
-import ioopm.mud.generalobjects.Character.Inventory;
-import ioopm.mud.generalobjects.ItemContainer;
-import ioopm.mud.generalobjects.Player;
-import ioopm.mud.generalobjects.Room;
-import ioopm.mud.generalobjects.World;
+import ioopm.mud.generalobjects.*;
 import ioopm.mud.generalobjects.items.Key;
 
 /**

src/main/java/ioopm/mud/game/See.java
@@ -6,7 +6,7 @@ import ioopm.mud.communication.messages.server.ErrorMessage;
 import ioopm.mud.communication.messages.server.ReplyMessage;
 import ioopm.mud.communication.messages.server.SeriousErrorMessage;
 import ioopm.mud.exceptions.EntityNotPresent;
-import ioopm.mud.generalobjects.Character.Inventory;
+//import ioopm.mud.generalobjects.Character.Inventory;
 import ioopm.mud.generalobjects.*;
 
 

src/main/java/ioopm/mud/generalobjects/Character.java
@@ -1,6 +1,7 @@
 package ioopm.mud.generalobjects;
 
 import ioopm.mud.generalobjects.items.Weapon;
+import ioopm.mud.generalobjects.*;
 
 /**
  * This class specifies a Character. Will be extended by other classes for PC's and NPC's.

src/test/java/tests/TestGameEngine.java
@@ -10,9 +10,7 @@ import ioopm.mud.communication.rawtcp.TCPAdapter;
 import ioopm.mud.exceptions.EntityNotPresent;
 import ioopm.mud.game.GameEngine;
 import ioopm.mud.game.Keywords;
-import ioopm.mud.generalobjects.Character.Inventory.InventoryOverflow;
-import ioopm.mud.generalobjects.ItemContainer;
-import ioopm.mud.generalobjects.World;
+import ioopm.mud.generalobjects.*;
 import ioopm.mud.generalobjects.worldbuilder.WorldBuilder;
 import ioopm.mud.generalobjects.worldbuilder.WorldBuilder.BuilderException;
 
@@ -52,7 +50,7 @@ public class TestGameEngine {
 	
 	
 	@Test
-	public void testEquipAndUnequip() throws BuilderException, EntityNotPresent, InventoryOverflow{
+	public void testEquipAndUnequip() throws BuilderException, EntityNotPresent, Inventory.InventoryOverflow{
 		makeMeAWorld();
 		ge = new GameEngine(adapter, world);
 		boolean p1;
@@ -545,7 +543,7 @@ public class TestGameEngine {
 	}
 	
 	@Test
-	public void testMove() throws BuilderException, EntityNotPresent, InventoryOverflow{
+	public void testMove() throws BuilderException, EntityNotPresent, Inventory.InventoryOverflow{
 		makeMeAWorld();
 		ge = new GameEngine(adapter, world);
 		adapter.flush();