2fb9b1e0ca772348f5cb232a806c3ad143ac3c9c
Lucas Arnström <lucasarnstrom@gmail.com>
2016-01-19 13:02:54 +0100

    Removed all SQL code

--- src/main/java/ioopm/mud/database/SQLite.java
+++ src/main/java/ioopm/mud/database/SQLite.java
@@ -15,98 +15,11 @@ import java.util.logging.Logger;
 
 public class SQLite implements PersistentStorage {
 
-	private static final String DATABASE_STRUCTURE =
-		// Tables
-		"CREATE TABLE IF NOT EXISTS player (" +
-			"id INTEGER PRIMARY KEY, " +
-			"username TEXT UNIQUE NOT NULL, " +
-			"location INTEGER REFERENCES room(id), " +
-			"password TEXT NOT NULL, " +
-			"salt TEXT, " + //TODO Set this field to not null when enabling hashing and salting
-			"is_admin BOOLEAN NOT NULL" +
-		");" +
+	private static final String DATABASE_STRUCTURE  = "";
 
-		"CREATE TABLE IF NOT EXISTS inventory (" +
-			"player_id INTEGER, " +
-			"volume INTEGER NOT NULL, " +
-			"max_volume INTEGER NOT NULL, " +
-			"FOREIGN KEY(player_id) REFERENCES player(id)" +
-		");" +
+	private static final String INSERT_PLAYER = "";
+	private static final String INSERT_CS = "";
 
-		"CREATE TABLE IF NOT EXISTS weapon (" +
-			"item_id INTEGER REFERENCES item(id), " +
-			"name TEXT UNIQUE NOT NULL, " +
-			"description TEXT NOT NULL, " +
-			"size INTEGER NOT NULL, " +
-			"level INTEGER NOT NULL, " +
-			"damage INTEGER NOT NULL" +
-		");" +
-
-		"CREATE TABLE IF NOT EXISTS key (" +
-			"item_id INTEGER REFERENCES item(id), " +
-			"target INTEGER REFERENCES room(id)" +
-		");" +
-
-		"CREATE TABLE IF NOT EXISTS character_sheet (" +
-			"player_id INTEGER UNIQUE NOT NULL, " +
-			"hp INTEGER, " +
-			"health INTEGER, " +
-			"max_health INTEGER, " +
-			"level INTEGER, " +
-			"FOREIGN KEY(player_id) REFERENCES player(id)" +
-		");" +
-
-		"CREATE TABLE IF NOT EXISTS room (" +
-			"id INTEGER PRIMARY KEY, " +
-			"name TEXT UNIQUE NOT NULL, " +
-			"description TEXT, " +
-			"pvp BOOLEAN NOT NULL" +
-		");" +
-
-		// M-N Relations
-		"CREATE TABLE IF NOT EXISTS inventory_item_relation (" +
-			"inv_id INTEGER, " +
-			"item_id INTEGER, " +
-			"amount INTEGER, " +
-			"FOREIGN KEY(inv_id) REFERENCES inventory(player_id), " +
-			"FOREIGN KEY(item_id) REFERENCES item(id), " +
-			"PRIMARY KEY(inv_id, item_id)" +
-		");" +
-
-		"CREATE TABLE IF NOT EXISTS room_item_relation (" +
-			"room_id INTEGER REFERENCES room(id), " +
-			"item_id INTEGER REFERENCES item(id), " +
-			"amount INTEGER NOT NULL" +
-		");" +
-
-		"CREATE TABLE IF NOT EXISTS room_exit_relation (" +
-			"room_id INTEGER REFERENCES room(id), " +
-			"exit_id INTEGER REFERENCES room(id)" +
-		");";
-
-	private static final String INSERT_PLAYER =
-		// This can be shorten to look like INSERT_CS
-		"INSERT OR REPLACE INTO player(id, username, location, password, is_admin) " +
-		"SELECT old.id, new.username, new.location, new.password, new.is_admin " +
-		"FROM ( SELECT " +
-			"? AS username, " +
-			"? AS location, " +
-			"? AS password, " +
-			"? AS is_admin " +
-		") AS new " +
-		"LEFT JOIN ( " +
-			"SELECT id, username " +
-			"FROM player " +
-		") AS old ON new.username = old.username;";
-
-	private static final String INSERT_CS =
-		"INSERT OR REPLACE INTO character_sheet(player_id, hp, health, max_health, level) " +
-		"VALUES ((" +
-			"SELECT id FROM player where player.username = ? " +
-		"), ?, ?, ?, ?) ";
-
-	private static final String INSERT_ITEM =
-		"INSERT ";
 
 	private static final Logger logger = Logger.getLogger(SQLite.class.getName());
 
@@ -152,54 +65,54 @@ public class SQLite implements PersistentStorage {
 
 	@Override
 	public void storePlayer(Player player) throws IllegalArgumentException {
-		try {
-			PreparedStatement stmt = database_connection.prepareStatement(INSERT_PLAYER);
+		//try {
+		//	PreparedStatement stmt = database_connection.prepareStatement(INSERT_PLAYER);
 
-			// Ugly as fuck reflection solution to password encapsulation.
-			String password;
-			try {
-				Field pass_field = Player.class.getDeclaredField("password");
-				pass_field.setAccessible(true);
-				password = (String) pass_field.get(player);
-			}
-			catch(NoSuchFieldException | IllegalAccessException e) {
-				logger.log(Level.SEVERE, e.getMessage(), e);
-				throw new IllegalArgumentException("Could not access password field in player! Check logs for stack trace.");
-			}
+		//	// Ugly as fuck reflection solution to password encapsulation.
+		//	String password;
+		//	try {
+		//		Field pass_field = Player.class.getDeclaredField("password");
+		//		pass_field.setAccessible(true);
+		//		password = (String) pass_field.get(player);
+		//	}
+		//	catch(NoSuchFieldException | IllegalAccessException e) {
+		//		logger.log(Level.SEVERE, e.getMessage(), e);
+		//		throw new IllegalArgumentException("Could not access password field in player! Check logs for stack trace.");
+		//	}
 
-			stmt.setString(1, player.getName());
-			stmt.setString(2, player.getLocation().getName());
-			stmt.setString(3, password);
-			stmt.setBoolean(4, player.isAdmin());
+		//	stmt.setString(1, player.getName());
+		//	stmt.setString(2, player.getLocation().getName());
+		//	stmt.setString(3, password);
+		//	stmt.setBoolean(4, player.isAdmin());
 
-			stmt.execute();
-			stmt.close();
-		}
-		catch(SQLException e) {
-			logger.log(Level.WARNING, e.getMessage(), e);
-			return;
-		}
+		//	stmt.execute();
+		//	stmt.close();
+		//}
+		//catch(SQLException e) {
+		//	logger.log(Level.WARNING, e.getMessage(), e);
+		//	return;
+		//}
 
-		storeCharacterSheet(player.getName(), player.getCs());
-		storeInventory(player.getInventory());
+		//storeCharacterSheet(player.getName(), player.getCs());
+		//storeInventory(player.getInventory());
 	}
 
 	private void storeCharacterSheet(String username, CharacterSheet cs) {
-		try {
-			PreparedStatement stmt = database_connection.prepareStatement(INSERT_CS);
+	//	try {
+	//		PreparedStatement stmt = database_connection.prepareStatement(INSERT_CS);
 
-			stmt.setString(1, username);
-			stmt.setInt(2, cs.getHp());
-			stmt.setInt(3, cs.getHealth());
-			stmt.setInt(4, cs.getMaxHealth());
-			stmt.setInt(5, cs.getLevel());
+	//		stmt.setString(1, username);
+	//		stmt.setInt(2, cs.getHp());
+	//		stmt.setInt(3, cs.getHealth());
+	//		stmt.setInt(4, cs.getMaxHealth());
+	//		stmt.setInt(5, cs.getLevel());
 
-			stmt.execute();
-			stmt.close();
-		}
-		catch(SQLException e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
+	//		stmt.execute();
+	//		stmt.close();
+	//	}
+	//	catch(SQLException e) {
+	//		logger.log(Level.SEVERE, e.getMessage(), e);
+	//	}
 	}
 
 	private void storeInventory(Inventory inventory) {
